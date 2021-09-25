59fa2bd91e31da60c1aac57edd02601f9765d7ce
Jacob Neilands <37431740+Wraith908@users.noreply.github.com>
2021-09-25 11:54:19 +1000

    Step 1

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -18,7 +18,7 @@ class Body extends Component {
         <Router>
           <header className = "navbar">
             <nav>
-              <Link to = "/" className = "nav-link">University Services</Link>
+              <Link to = "/" className = "nav-link"><img src = "logo.png" /></Link>
               <Link to = "/directory" className = "nav-link">Staff Directory</Link>
               <Link to = "/week-one-survival-pack" className = "nav-link">Week 1 Survival Pack</Link>
               <Link to = "/university-expectations" className = "nav-link">What to expect at University</Link>
